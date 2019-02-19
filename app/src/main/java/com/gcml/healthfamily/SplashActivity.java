4f0fccc7318e948d520ef727b5b5cbbaf4d341cf
gzq <774550196@qq.com>
2019-02-19 10:06:19 +0800

    优化启动速度

--- app/src/main/java/com/gcml/healthfamily/SplashActivity.java
+++ app/src/main/java/com/gcml/healthfamily/SplashActivity.java
@@ -5,7 +5,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.githang.statusbar.StatusBarCompat;
-import com.gzq.lib_resource.utils.WeakHandler;
 import com.sjtu.yifei.route.Routerfit;
 
 public class SplashActivity extends AppCompatActivity {
@@ -13,12 +12,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarCompat.setTranslucent(getWindow(), true);
-        new WeakHandler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Routerfit.register(AppRouterApi.class).skipLoginActivity();
-                finish();
-            }
-        }, 100);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Routerfit.register(AppRouterApi.class).skipLoginActivity();
+        finish();
     }
 }
